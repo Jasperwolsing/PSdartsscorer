c550828609155744be203d5f5483ba3f84aa5c21
Jasperwolsing <wolsing@outlook.com>
2020-03-15 13:46:51 +0100

    Update updater.ps1

--- updater.ps1
+++ updater.ps1
@@ -5,13 +5,14 @@ $version = Get-Content C:\temp\repository.version
 $installedversion = Get-Content C:\Scorebord\installed.version -ErrorAction SilentlyContinue
 if(!$installedversion -and $installedversion -lt $version)
 {
-    $URLupdater = 
+    $URLupdater = "https://raw.githubusercontent.com/Jasperwolsing/PSdartsscorer/master/updater.ps1"
+    $updaterlocation = "C:\Scorebord\updater.ps1" 
+    $URL1 = "https://raw.githubusercontent.com/Jasperwolsing/PSdartsscorer/master/Scorebord.ps1"
+    $url1location = "C:\scorebord\scorebord.ps1"
 
+
+    Echo $version > C:\Scorebord\installed.version
 }
 
 
-$URL1 = "https://raw.githubusercontent.com/Jasperwolsing/PSdartsscorer/master/Scorebord.ps1"
-
-
-Echo $version > C:\Scorebord\installed.version
 
